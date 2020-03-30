46d6c378956f9554c66afcf907058400ae3a1f7c
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2020-03-30 12:59:06 +0530

    echo command

---
+++ second.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#this is a comment
+echo "Hello   World"
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello    World
+echo "hello" World
+echo hello "    " World
+echo "hello "*" world"
+echo 'hello' world
+echo `hello` world 
